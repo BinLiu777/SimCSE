2a0d2270e2a13cf6215db261d4af724b8e4a5762
liubin13720 <liubin13720@ipalfish.com>
2021-07-25 11:29:31 +0800

    add file

---
+++ data_process.py
@@ -0,0 +1,36 @@
+def saller():
+    with open('data/saller/saler_pure_text_nocut.txt', 'r') as f:
+        lines = f.readlines()
+    print(len(lines))
+    res = []
+    with open('data/saller/saler_pure_text_nocut_filter.txt', 'w') as f:
+        for line in lines:
+            text = line.strip()
+            text = text.replace('。', '')
+            text = text.replace('，', '')
+            text = text.replace('？', '')
+            text = text.replace('！', '')
+            if len(text) > 5:
+                f.write(line)
+                res.append(line)
+    print(len(res))
+
+def best_saller():
+    with open('data/best_saller/sale_asr_2000_single_poun', 'r') as f:
+        lines = f.readlines()
+        print(len(lines))
+    res = []
+    with open('data/best_saller/sale_asr_2000_pure_text.txt', 'w') as f:
+        for line in lines:
+            if "result_final_train" not in line:
+                text, role = line.strip().split('\t')
+                text_ = text.replace('。', '')
+                text_ = text_.replace('，', '')
+                text_ = text_.replace('？', '')
+                text_ = text_.replace('！', '')
+                if len(text_) > 1:
+                    f.write(text + '\n')
+                    res.append(line)
+    print(len(res))
+
+best_saller()
