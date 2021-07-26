6a1e9d441ce9660f0aab0f0384ba1b2e77206c31
liubin13720 <liubin13720@ipalfish.com>
2021-07-26 19:47:50 +0800

    modify simCSE

--- eval.sh
+++ eval.sh
@@ -1,5 +1,6 @@
 python evaluation.py \
-    --model_name_or_path princeton-nlp/sup-simcse-bert-base-uncased \
+    --model_name_or_path result/my-unsup-simcse-bert-base-chinese-webank \
     --pooler cls \
     --task_set sts \
     --mode test
+#    --model_name_or_path princeton-nlp/sup-simcse-bert-base-uncased \
