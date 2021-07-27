c63e7ab61d7c81aab608f02bb18d339f8ff9ee52
liubin13720 <liubin13720@ipalfish.com>
2021-07-27 20:50:00 +0800

    modify simCSE

--- run_unsup_example.sh
+++ run_unsup_example.sh
@@ -6,9 +6,9 @@
 #python -m torch.distributed.launch --nproc_per_node=4 train.py \
 CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 train.py \
     --model_name_or_path bert-base-chinese \
-    --train_file data/webank/webank_pure_text_nocut_train.txt \
-    --output_dir result/my-unsup-simcse-bert-base-chinese-webank-2 \
-    --num_train_epochs 10 \
+    --train_file data/saller/asr_saller.txt \
+    --output_dir result/saller_totol \
+    --num_train_epochs 3 \
     --per_device_train_batch_size 64 \
     --learning_rate 3e-5 \
     --max_seq_length 32 \
@@ -19,7 +19,7 @@ CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --nproc_per_node=1 tra
     --overwrite_output_dir \
     --temp 0.05 \
     --do_train \
-    --eval_steps 125 \
+    --eval_steps 500 \
     --evaluation_strategy steps \
     --do_eval \
     --fp16 \
