bdd02961897bbb8a28895d6af25148d148d4cc35
liubin13720 <liubin13720@ipalfish.com>
2021-07-28 15:41:30 +0800

    modify simCSE

--- run_unsup_example.sh
+++ run_unsup_example.sh
@@ -8,8 +8,8 @@ PORT_ID=$(expr $RANDOM + 1000)
 
 python -m torch.distributed.launch --nproc_per_node $NUM_GPU --master_port $PORT_ID train.py \
     --model_name_or_path bert-base-chinese \
-    --train_file data/saller/asr_saller.txt \
-    --output_dir result/unsup/saller_webank \
+    --train_file data/webank/webank_pure_text_nocut_train.txt \
+    --output_dir result/unsup/webank_webank \
     --num_train_epochs 10 \
     --per_device_train_batch_size 64 \
     --learning_rate 3e-5 \
@@ -21,7 +21,7 @@ python -m torch.distributed.launch --nproc_per_node $NUM_GPU --master_port $PORT
     --overwrite_output_dir \
     --temp 0.05 \
     --do_train \
-    --eval_steps 500 \
+    --eval_steps 200 \
     --evaluation_strategy steps \
     --do_eval \
     --fp16 \
