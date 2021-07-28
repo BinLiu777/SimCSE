a8497c1bbe781715873ecd57a1ed811875893b5f
liubin13720 <liubin13720@ipalfish.com>
2021-07-28 21:42:27 +0800

    modify simCSE

--- SimCSE_chinese/train_unsupervised.py
+++ SimCSE_chinese/train_unsupervised.py
@@ -66,8 +66,8 @@ simCSE_data = np.random.choice(all_vocab, 150000)
 print(len(simCSE_data))
 test_data = load_STS_data(os.path.join(sts_file_path, sts_test_file))
 dev_data = load_STS_data(os.path.join(sts_file_path, sts_dev_file))
-test_data = np.random.choice(test_data, 1000)
-dev_data = np.random.choice(dev_data, 1000)
+test_data = test_data[:1000]
+dev_data =dev_data[-1000:]
 print(len(test_data))
 print(len(dev_data))
 
