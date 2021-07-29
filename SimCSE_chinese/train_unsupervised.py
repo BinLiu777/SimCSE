0cb963d39c4fc744a78ea72414a344d21884651e
liubin13720 <liubin13720@ipalfish.com>
2021-07-29 14:28:37 +0800

    modify modify seq2vec

--- SimCSE_chinese/train_unsupervised.py
+++ SimCSE_chinese/train_unsupervised.py
@@ -9,21 +9,25 @@ import scipy.stats
 from tqdm import tqdm
 import os
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+output_way = 'pooler'
+assert output_way in ['pooler', 'cls', 'avg']
+
+# model_path = "bert-base-uncased"
+model_name = 'bert'
+assert model_name in ['bert', 'roberta']
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 print("Using {} device".format(device))
-# model_path = "bert-base-uncased"
-# model_path = "hfl/chinese-bert-wwm-ext"
-model_path = 'hfl/chinese-roberta-wwm-ext'
-# save_path = "./model_saved/saller/best_model_bert_wwm_ext_cls.pth"
-save_path = "./model_saved/saller/best_model_roberta_wwm_ext_pooler.pth"
+
+model_path = f"hfl/chinese-{model_name}-wwm-ext"
+save_path = f"./model_saved/saller/best_model_{model_name}_wwm_ext_{output_way}.pth"
+
 tokenizer = BertTokenizer.from_pretrained(model_path)
 Config = BertConfig.from_pretrained(model_path)
 Config.attention_probs_dropout_prob = 0.3
 Config.hidden_dropout_prob = 0.3
 
-output_way = 'pooler'
-assert output_way in ['pooler', 'cls', 'avg']
 
 sts_file_path = "./datasets/STS-B/"
 sts_train_file = 'cnsd-sts-train.txt'
