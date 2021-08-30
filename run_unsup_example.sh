1e9f58828a7a076dda4042ddbc936035e71cb752
liubin13720 <liubin13720@ipalfish.com>
2021-08-30 21:05:46 +0800

    modify

--- run_unsup_example.sh
+++ run_unsup_example.sh
@@ -30,21 +30,21 @@ NUM_GPU=1
 PORT_ID=$(expr $RANDOM + 1000)
 
 python -m torch.distributed.launch --nproc_per_node $NUM_GPU --master_port $PORT_ID train.py \
-    --model_name_or_path bert-base-uncased \
-     --train_file data/wiki1m_for_simcse.txt \
-     --output_dir result/unsup/wiki1m_littlelr \
-    --num_train_epochs 5 \
-    --per_device_train_batch_size 64 \
+    --model_name_or_path bert-base-chinese \
+    --train_file data/saller_good_ppl.txt \
+    --output_dir result/unsup/saller_saller \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 512 \
     --learning_rate 5e-5 \
-    --max_seq_length 32 \
+    --max_seq_length 64 \
     --metric_for_best_model stsb_spearman \
     --load_best_model_at_end \
-    --pooler_type cls \
+    --pooler_type cls_before_pooler \
     --mlp_only_train \
     --overwrite_output_dir \
     --temp 0.05 \
     --do_train \
-    --eval_steps 250 \
+    --eval_steps  \
     --evaluation_strategy steps \
     --do_eval \
     --fp16 \
